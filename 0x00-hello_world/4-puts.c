1e2d7dbb41752257bb6540dd7d9c049984c512f4
Noah Chernet <noahchernet@yahoo.com>
2021-08-26 13:53:48 +0300

    Added 4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/*Prints a string*/
+int main(void)
+{
+	/*Prints a string*/
+	char myStr[] = "\"Programming is like building a multilingual puzzle\n";
+
+	puts(myStr);
+	return (0);
+}
