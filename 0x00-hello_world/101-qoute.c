b0deb2b1a7c7e9f451c093beee967ca696f47d59
Noah Chernet <noahchernet@yahoo.com>
2021-08-26 18:30:15 +0300

    101-qoute.c added.

--- 0x00-hello_world/101-qoute.c
+++ 0x00-hello_world/101-qoute.c
@@ -8,8 +8,7 @@
 
 int main(void)
 {
-fprintf(stderr, "and that piece of art is useful\" - Dora Korpar, 2015-10-19",
-"string format", 30);
+fprintf(stderr, "%s", "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
 
 return (1);
 }
