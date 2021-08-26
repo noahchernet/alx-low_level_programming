87506ee07244324132bf3737d729b7211c317033
Noah Chernet <noahchernet@yahoo.com>
2021-08-26 18:12:09 +0300

    101-quote.c added.

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/*
+ * main - Prints text to standard error
+ *
+ * Return: 1 error has been caught
+ */
+int main(void)
+{
+	fprintf( stderr, "and that piece of art is useful\" - Dora Korpar, 2015-10-19");
+
+	return (1);
+}
