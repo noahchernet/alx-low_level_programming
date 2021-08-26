948545177ee6aa9893a5e9d7d1fa076965b0bfd7
Noah Chernet <noahchernet@yahoo.com>
2021-08-26 20:26:22 +0300

    101-quote.c added.

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 
 /**
- * main - Prints text to standard error
- *
- * Return: 1, error has been caught
+ * main - Entry point
+ *  
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
-	printf("and that piece of art is useful\" - Dora Korpar, 2015-10-19");
+	printf("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
 	return (1);
 }
