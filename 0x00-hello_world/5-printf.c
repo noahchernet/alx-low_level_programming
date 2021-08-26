e73ca8df15b1b9e44bc9ffdfaf0b875a3fb757c8
Noah Chernet <noahchernet@yahoo.com>
2021-08-26 16:36:58 +0300

    5-printf.c added.

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - Program starts here
+ *
+ * Return: 0, not necessaary to return another value
+ */
+int main(void){
+	printf ("with proper grammar, but the outcome is a piece of art,\n");
+
+	return (0);
+}
