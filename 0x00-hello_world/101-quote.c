6c53e9dc197cc6eabc108c1233c593df70658d6e
Noah Chernet <noahchernet@yahoo.com>
2021-08-26 20:23:15 +0300

    101-quote.c added.

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -5,10 +5,8 @@
  *
  * Return: 1, error has been caught
  */
-
 int main(void)
 {
-	fprintf("and that piece of art is useful\" - Dora Korpar, 2015-10-19");
-
+	printf("and that piece of art is useful\" - Dora Korpar, 2015-10-19");
 	return (1);
 }
