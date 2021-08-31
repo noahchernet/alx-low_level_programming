3f10ae4094d7f386812be0a64f74569efa0a45af
Noah Chernet <noahchernet@yahoo.com>
2021-08-31 16:18:25 +0300

    0-holberton.c added.

--- 0x02-functions_nested_loops/0-holberton.c
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -7,15 +7,19 @@
  */
 int main(void)
 {
-	/* int Holberton[] = {72, 111, 108, 98, 101, 114, 116,
-			111, 110};*/
-	char Holberton[] = "_putchar";
+	/*
+	 * int Holberton[] = {72, 111, 108, 98, 101, 114, 116,
+			111, 110};
+	*/
+	char Holberton[] = "_putchar\n";
 	int ij;
 
 	for (ij = 0; ij < 9; ij++)
 	{
 		_putchar(Holberton[ij]);
 	}
-	_putchar(10);
+	/*
+	 * _putchar(10);
+	 */
 	return (0);
 }
