ac9b8ec3c55cc3c6594b91968217ba277cf7c846
Noah Chernet <noahchernet@yahoo.com>
2021-08-31 15:29:04 +0300

    0-holberton.c added.

---
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -0,0 +1,18 @@
+#include "_putchar.c"
+
+/**
+ * main - Prints Holberton using _putchar()
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int chars[] = {72, 111, 108, 98, 101, 114, 116,
+	111, 110};
+	for (int i = 0; i < 9; i++)
+	{
+		_putchar(chars[i]);
+	}
+	_putchar(10);
+	return (0);
+}
