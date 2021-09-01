fc9e7c38f8fc59e8ed73ab8c2c03752b754ef341
Noah Chernet <noahchernet@yahoo.com>
2021-09-01 23:36:42 +0300

    11-print_to_98.c added.

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -12,18 +12,20 @@ void print_to_98(int n)
 	{
 	for (; n >= 98; n--)
 	{
-		printf("%d, ", n);
+		printf("%d", n);
 		if (n == 98)
 			break;
+		printf(", ");
 	}
 	}
 	else
 	{
 	for (; n <= 98; n++)
 	{
-		printf("%d, ", n);
+		printf("%d", n);
 		if (n == 98)
 			break;
+		printf(", ");
 	}
 	}
 	printf("\n");
