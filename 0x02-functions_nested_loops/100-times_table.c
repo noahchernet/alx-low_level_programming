53d05d3fbe9c94ac44cb9379691e95372a10b7bb
Noah Chernet <noahchernet@yahoo.com>
2021-09-01 22:08:00 +0300

    100-times_table.c added.

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -13,9 +13,9 @@ void print_times_table(int n)
 	{
 		return;
 	}
-	for (i = 0; i <= 9; i++)
+	for (i = 0; i <= n; i++)
 	{
-		for (j = 0; j <= 9; j++)
+		for (j = 0; j <= n; j++)
 		{
 			if (((i * j) / 10) % 10 != 0)
 			{
