a3bfec3742edda1ac0a6778e0b833aababc4132d
Noah Chernet <noahchernet@yahoo.com>
2021-09-01 23:00:43 +0300

    11-print_to_98.c added.

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * print_to_98 - prints natural numbers from n to 98, followed by a new line.
+ * @n: the first number to be printed on screen
+ *
+ * Return: void
+ */
+void print_to_98(int n)
+{
+	for (; n <= 98; n++)
+	{
+		_putchar((n / 10) % 10);
+		_putchar(n % 10);
+
+		if (n == 98)
+			return;
+
+		putchar(44);
+		_putchar(32);
+	}
+}
