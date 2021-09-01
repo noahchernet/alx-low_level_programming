427de68c4e06f35e7b248449efca9409995df20d
Noah Chernet <noahchernet@yahoo.com>
2021-09-01 22:37:13 +0300

    10-add.c added.

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * add - adds two numbers, int a and int b
+ * @a: the first number to be added
+ * @b: the second number to be added
+ * Return: @a + @b, the sum of the integers
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
