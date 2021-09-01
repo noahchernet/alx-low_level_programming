1ab92fed27fa3f005cef320fdb093bc45ae220e1
Noah Chernet <noahchernet@yahoo.com>
2021-09-01 17:33:01 +0300

    5-sign.c added.

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * print_sign - Prints the sign of the number passed in
+ * @n: the number whose sign is to be checked
+ *
+ * Return: 1 and prints + if n is greater than zero
+ *	   0 and prints 0 if n is zero
+ *	  -1 and prints - if n is less than zero
+ */
+
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		return (43);
+	}
+	else if (n == 0)
+	{
+		return (0);
+	}
+	else
+	{
+		return (45);
+	}
+}
