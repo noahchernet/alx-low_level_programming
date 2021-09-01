cddd9bb0e6d60768cff9856530c08a8fb24e47bb
Noah Chernet <noahchernet@yahoo.com>
2021-09-01 16:55:01 +0300

    1-alphabet.c added.

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * main - prints the alphabet in lower case
+ *  print_alphabet - prints the alphabet in lower case
  *
  * Return: 0
  */
@@ -9,7 +9,8 @@ void print_alphabet(void)
 {
 	char lowerCase[] = "abcdefghijklmnopqrstuvwxyz";
 	int i;
-	for(i = 0; i < 26; i++)
+
+	for (i = 0; i < 26; i++)
 	{
 		_putchar(lowerCase[i]);
 	}
