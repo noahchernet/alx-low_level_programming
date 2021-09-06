0da7b5f20ccdb224acf76d1c993123a0f33d57f5
Noah Chernet <noahchernet@yahoo.com>
2021-09-06 22:24:15 +0300

    6-puts2.c added.

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -11,9 +11,16 @@
 void puts2(char *str)
 {
 	int i;
+	int n = 0;
 
-	for (i = 0; str[i] != 0; i += 2)
+	while (str[n] != 0)
 	{
+		n++;
+	}
+
+	for (i = 0; i <= n; i += 2)
+	{
+		if (str[i] != 0)
 		_putchar(str[i]);
 	}
 	_putchar(10);
