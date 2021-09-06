101db5b3d38c42ce2586a873c8e8dbe5336ea098
Noah Chernet <noahchernet@yahoo.com>
2021-09-06 21:04:25 +0300

    2-strlen.c added.

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _strlen - counts the length of a string
+ * @s: the string whose length is to be calculated
+ *
+ * Return: n, the length of the sring
+ */
+
+int _strlen(char *s)
+{
+	int n = 0;
+
+	while (s[n] != 0)
+	{
+		n++;
+	}
+
+	return (n++);
+}
