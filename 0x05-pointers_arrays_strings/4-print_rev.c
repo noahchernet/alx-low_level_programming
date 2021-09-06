0f3ce815c9a70fd7750ffb2c28d4e7a187b96b0f
Noah Chernet <noahchernet@yahoo.com>
2021-09-06 21:23:20 +0300

    4-print_rev.c added.

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * print_rev - prints a string in reverse followed by a new line
+ * @s: the string to be reversed
+ *
+ * Return: void
+ */
+
+void print_rev(char *s)
+{
+	int n = 0; /* The size of the string */
+
+	/* Calculate the size of the string */
+	while (s[n] != 0)
+	{
+		n++;
+	}
+
+	while (n  >= 0)
+	{
+		_putchar(s[n]);
+		n--;
+	}
+	_putchar(10);
+}
