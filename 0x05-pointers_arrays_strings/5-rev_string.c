6208764a931c37de39cc4f1886ca47346ed6f867
Noah Chernet <noahchernet@yahoo.com>
2021-09-07 20:02:58 +0300

    5-rev_string.c added.

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -1,32 +1,50 @@
 #include "main.h"
 
 /**
+ * swap - swaps two values in an int array
+ * @list: the array whose values are to be swapped
+ * @a: first value to be swapped to the second value
+ * @b: second value to be swapped to the first value
+ *
+ * Return: void
+ */
+void swap(char *list, int a, int b)
+{
+	char temp = list[a];
+
+	list[a] = list[b];
+	list[b] = temp;
+}
+
+/**
  * rev_string - prints a string
  * @s: the string to be reversed
  *
  * Return: void
  */
-
 void rev_string(char *s)
 {
-	int n = 0; /* The size of the string */
+	int n = 0; /* Holds the size of s */
 	int i;
-	char *r;
 	int k;
 
-	r = s;
-
-	/* Calculate the size of the string */
-	while (s[n] != 0)
+	/* Calculate the size of n */
+	while (s[n] != '\0')
 	{
 		n++;
 	}
 
+	n--;
 	k = n;
 
-	for (i = 0; i <= k; i++)
+	for (i = 0; i <= (n / 2); i++)
 	{
-		s[i] = r[n];
-		n--;
+		/*
+		 * printf("Swapping \'%c\' with \'%c\'\n", s[i], s[k]);
+		 * printf("s is now: %s\n", s);
+		 */
+		swap(s, i, k);
+		k--;
 	}
+
 }
