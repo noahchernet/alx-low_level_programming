653b92bbcce272e7886991cbbdea49b23e78f92d
Noah Chernet <noahchernet@yahoo.com>
2021-09-11 15:33:11 +0300

    101-print_number.c added.

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -1,5 +1,4 @@
 #include "main.h"
-#include <stdio.h>
 
 /**
   * print_number - prints the passed intiger using _putchar
@@ -11,13 +10,13 @@
 void print_number(int n)
 {
 	int i = 1;
-
+/*
 	if (n == 0)
 	{
 		_putchar(48);
 		return;
 	}
-
+*/
 	if (n < 0)
 	{
 		n *= -1;
@@ -27,12 +26,16 @@ void print_number(int n)
 	while (n / i != 0)
 		i *= 10;
 	i /= 10;
-/*	printf("i is %d\n", i);*/
 
 	while (i != 0)
 	{
 		_putchar(((n / i) % 10) + 48);
 		i /= 10;
 
+	}
+	
+	if (n == 0)
+	{
+		_putchar(48);
 	}
 }
