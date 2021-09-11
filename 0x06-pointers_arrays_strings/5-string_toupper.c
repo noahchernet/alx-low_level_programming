2244d454cbec6334ed87766271ce58d7fdd65c30
Noah Chernet <noahchernet@yahoo.com>
2021-09-11 11:36:22 +0300

    5-string_toupper.c added.

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * string_toupper - converts all lowercase characters to uppercase
+ * @s: string to be converted to uppercase
+ *
+ * Return: @str, the uppercased string
+ */
+
+char *string_toupper(char *s)
+{
+	int len = 0;
+	int i;
+
+	while (s[len] != '\0')
+	{
+		len++;
+	}
+
+	for (i = 0; i < len; i++)
+	{
+		if (s[i] >= 97 && s[i] <= 122)
+			s[i] = (char) s[i] - 32;
+	}
+
+	return (s);
+}
