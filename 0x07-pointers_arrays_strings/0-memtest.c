5f78c553e2be92db2de97680ac3141c8022e2642
Noah Chernet <noahchernet@yahoo.com>
2021-09-14 13:18:38 +0300

    Converted spaces to tabs

--- 0x07-pointers_arrays_strings/0-memtest.c
+++ 0x07-pointers_arrays_strings/0-memtest.c
@@ -12,12 +12,12 @@
 
 char *_memset(char *s, char b, unsigned int n)
 {
-    int i;
+	int i;
 
-    for (i = 0; i < n; ++i)
-    {
-        s[i] = b;
-    }
+	for (i = 0; i < n; ++i)
+	{
+		s[i] = b;
+	}
 
-    return (s);
+	return (s);
 }
