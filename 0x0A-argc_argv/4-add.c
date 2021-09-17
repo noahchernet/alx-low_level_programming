ac521829102144df2cc289460105c3a0f5b21c1b
Noah Chernet <noahchernet@yahoo.com>
2021-09-17 17:48:06 +0300

    4-add.c added.

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -16,6 +16,12 @@ int main(int argc, char *argv[])
 	int i;
 	int sum = 0;
 
+	if (argc == 1)
+	{
+		printf("0\n");
+		return (0);
+	}
+
 	for (i = 1; i < argc; i++)
 	{
 		if (atoi(argv[i]) == 0 && *argv[i] != '0')
