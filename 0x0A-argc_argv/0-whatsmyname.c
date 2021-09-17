002c486d4f6696fdf1daa02a202b090567ad8d03
Noah Chernet <noahchernet@yahoo.com>
2021-09-17 15:49:14 +0300

    0-whatsmyname.c added.

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+  * main - prints the name passed to it on runtime
+  * @argc: number of arguments passed
+  * @argv: array of passed arguments
+  *
+  * Return: Always 0
+  */
+
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
