dd5b8ccd615b96e9ebd1b7032cb912adc4a1d383
Noah Chernet <noahchernet@yahoo.com>
2021-09-18 23:21:44 +0300

    First trial of 100-prime_factor.c

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -1,14 +1,25 @@
 #include <stdio.h>
 
 /**
-* main - prints the largest prime factor of number 612852475143
+ * main - prints the largest prime factor of number 612852475143
  *
  * Return: Always 0
 */
 
-int main()
+int main(void)
 {
-	printf("hi");
+	int i;
+	double num = 612852475143.0;
+
+	for (i = 2; i < num / 2; i++)
+	{
+		/* printf("Now, i is %d and num / i is: %f\n", i, (num / i)); */
+		if (num / i == (int) (num / i))
+		{
+			printf("%d\n", (int) (num / i));
+			break;
+		}
+	}
 
 	return (0);
 }
