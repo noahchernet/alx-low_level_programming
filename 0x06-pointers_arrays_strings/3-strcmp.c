5533e8d869b05c8f06be579ad29dec605e48a872
Noah Chernet <noahchernet@yahoo.com>
2021-09-19 20:29:27 +0300

    Program's results identical to task description in this update

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -1,42 +1,24 @@
 #include "main.h"
-#include <stdio.h>
 
 /**
  * _strcmp - compares two strings
  * @s1: string to be compared against @s2
- * @s2: the sencond string used to compare with @s1
+ * @s2: the second string used to compare with @s1
  *
  * Return: the sum of the difference of each characters of @s1 and @s2
  */
 
 int _strcmp(char *s1, char *s2)
 {
-	int s1_len = 0;
-	int s2_len = 0;
-	int comp = 0;
-	int i;
+	int i = 0;
 
-	while ( s1[s1_len] != '\0')
+	while (s1[i] != 0 && s2[i] != 0 && s1[i] == s2[i])
 	{
-		s1_len++;
+		i++;
 	}
 
-	while (s2[s2_len] != '\0')
-	{
-		s2_len++;
-	}
+	if (s1[i] != 0 && s2[i] != 0)
+		return (s1[i] - s2[i]);
 
-	/* printf("s1_len: %d, s2_len: %d\n", s1_len, s2_len);*/
-
-	for (i = 0; i < s2_len; i++)
-	{
-		if ((int) s2[i] > (int) s1[i])
-			comp--;
-		else if ((int) s2[i] == (int) (s1[i]))
-			continue;
-		else
-			comp++;
-	}
-
-	return (comp);	
+	return (0);
 }
