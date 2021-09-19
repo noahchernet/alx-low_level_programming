53755020355544dc6c21ba6fbef2cac1ba4e2c0c
Noah Chernet <noahchernet@yahoo.com>
2021-09-19 20:49:44 +0300

    Changed max value of k from 14 to 13. Size of @c is 13.

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -13,7 +13,7 @@ char *cap_string(char *s)
 	/*
 	 * @c: the characters that make the next letter eligible to be capitalized
 	 */
-	char c[] = {' ', '\t', '\n', ',', ';', '.', '!', '?', '"', '(', ')',
+	char c[] = {' ', '\t', '\n', ',', ';', '.', '!', '?', '\"', '(', ')',
 		  '{', '}'};
 	int len = 0;
 	int i;
@@ -27,7 +27,7 @@ char *cap_string(char *s)
 
 	for (i = 0; i < len; i++)
 	{
-		for (k = 0; k < 14; k++)
+		for (k = 0; k < 13; k++)
 		{
 			if (s[i] == c[k] && s[i + 1] >= 97 && s[i + 1] <= 122)
 			{
