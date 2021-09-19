90fe90a6d5245984e3806876cc41a075b97905eb
Noah Chernet <noahchernet@yahoo.com>
2021-09-19 13:39:42 +0300

    Using sieve of atkins to find primes, yet to be finished.

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -1,6 +1,59 @@
 #include <stdio.h>
 
 /**
+ * seive_of_atkin - lists all the prime numbers up to limit 612852475143
+ *
+ * Return: array of primes
+ */
+
+void seive_of_atkin()
+{
+	long limit = 612852475143;
+	static int seive[50847534];
+	long i, n, x, y, r;
+
+	for (i = 0; i < limit; i++)
+	{
+		seive[i] = 0;
+	}
+	for (x = 1; x * x < limit; x++)
+	{
+		for (y = 1; y * y < limit; y++)
+		{
+			n = (4 * x * x) + (y * y);
+			if (n <= limit && (n % 12 == 1 || n % 12 == 5))
+				seive[n] ^= 1;
+
+			n = (3 * x * x) + (y * y);
+			if (n <= limit && n % 12 == 7)
+				seive[n] ^= 1;
+
+			n = (3 * x * x) - (y * y);
+			if (x > y && n <= limit && n % 12 == 11)
+				seive[n] ^= 1;
+		}
+
+/*		 Mark all multiples of squares as non-prime*/
+		for (r = 5; r * r < limit; r++)
+			if (seive[r])
+			{
+				for (i = r * r; i < limit; i += r * r)
+					seive[i] = 0;
+			}
+
+	}
+
+	/* Print primes using seive[]*/
+	for (i = 5; i < limit; i++)
+	{
+		if (seive[i])
+			printf("%ld", i);
+	}
+
+/*	return (seive);*/
+}
+
+/**
  * main - prints the largest prime factor of number 612852475143
  *
  * Return: Always 0
@@ -8,18 +61,8 @@
 
 int main(void)
 {
-	int i;
-	double num = 612852475143.0;
-
-	for (i = 2; i < num / 2; i++)
-	{
-		/* printf("Now, i is %d and num / i is: %f\n", i, (num / i)); */
-		if (num / i == (int) (num / i))
-		{
-			printf("%d\n", (int) (num / i));
-			break;
-		}
-	}
-
+	printf("Starting...\n");
+	seive_of_atkin();
+	printf("Finished\n");
 	return (0);
 }
