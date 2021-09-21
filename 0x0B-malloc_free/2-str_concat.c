d28a4027cfbcf4adac101c1efe614e169111ac8b
Noah Chernet <noahchernet@yahoo.com>
2021-09-21 17:46:08 +0300

    Program output matches task description

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,41 @@
+#include "main.h"
+
+/**
+ * str_concat - concatenates @s1 and @s2 and stores the new string in a new
+ *				location in the memory
+ * @s1: string where @s2 will be concatenated to
+ * @s2: string to be concatenated on @s1
+ * Return: @s1 + @s2
+ *		   NULL if function fails
+ */
+char *str_concat(char *s1, char *s2)
+{
+	int len_s1 = 0, len_s2 = 0, n;
+	char *concatenated_str;
+
+	while (s1[len_s1] != 0)
+		len_s1++;
+
+	while (s2[len_s2] != 0)
+		len_s2++;
+
+	n = len_s1 + len_s2;
+	concatenated_str = malloc(n);
+
+	n = len_s1;
+	while (n >= 0)
+	{
+		concatenated_str[n] = s1[n];
+		n--;
+	}
+
+	n = len_s2;
+	while (n >= 0)
+	{
+		concatenated_str[len_s1 + n] = s2[n];
+		n--;
+	}
+
+	return (concatenated_str);
+
+}
