a5da5064bb244d66b6e80b68b00c2c2d51a2fa89
Noah Chernet <noahchernet@yahoo.com>
2021-09-21 23:34:13 +0300

    Added 4-free_grid.c

---
+++ 0x0B-malloc_free/4-free_grid.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+* free_grid - frees the 2D array allocated by alloc_grid
+ * @grid: the address of the bytes to be freed
+ * @height: the size of the grid array
+ *
+ * Return: void
+*/
+void free_grid(int **grid, int height)
+{
+	int i;
+
+	for (i = 0; i < height - 1; i++)
+		free(grid[i]);
+
+	free(grid);
+
+}
