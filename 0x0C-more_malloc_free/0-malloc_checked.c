a428bef55cc19882c53a83cf580c87c68da288a0
Noah Chernet <noahchernet@yahoo.com>
2021-09-23 22:18:58 +0300

    Updated 0-malloc_checked.c to exit with status 98 if malloc failed to allocate memory.

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -4,13 +4,16 @@
  * malloc_checked - returns pointer to allocated memory
  * @b: bytes to be allocated
  * Return: pointer to the address of the allocated memory
- *		   NULL if malloc fails to allocate memory
+ *		   98 if malloc fails to allocate memory
  */
 void *malloc_checked(unsigned int b)
 {
-	void *allocated_mem = malloc(b);
+	void *allocated_mem;
+	allocated_mem =  malloc(b);
 
-	if (!allocated_mem)
-		return ((void *) 98);
-	return (malloc(b));
+	if (allocated_mem)
+		return (allocated_mem);
+
+	exit(98);
+
 }
