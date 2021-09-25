d30b5e8e4d670db930637bc0bdff3a807f141254
Noah Chernet <noahchernet@yahoo.com>
2021-09-26 01:22:45 +0300

    Updated 3-array_range.c and main.h. Added function abs()

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -1,6 +1,22 @@
 #include "main.h"
 
 /**
+ * abs - calculates the absolute value of passed integer
+ * @a: the integer whose absolute value will be calculated
+ * Return: if a < 0, the negation of a i.e. -1 * a
+ *		   otherwise return a without modification
+ */
+int abs(int a)
+{
+	if (a < 0)
+	{
+		a *= -1;
+		return (-1 * a);
+	}
+	return (a);
+}
+
+/**
  * array_range - creates an array of integers between min and max (including
  * both min and max)
  * @min: the smallest and first integer in the array
@@ -15,17 +31,23 @@ int *array_range(int min, int max)
 {
 	int *arr;
 	int i;
-	int j = max - min;
+	int j = abs(max) - abs(min);
+
+	j = abs(j);
 
 	if (min > max)
 		return (NULL);
 
+
+/*
 	if ((min < 0) && (max < 0))
 		j = (-1 * max) - (-1 * min);
 	else if ((max < 0) && (min > 0))
 		j = (-1 * max) - min;
 	else if ((min < 0) && (max > 0))
 		j = max - (-1 * min);
+*/
+
 
 	arr = malloc(sizeof(int) * (max - min) + 1);
 
