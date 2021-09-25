c8925d563b0c862c925efc1ac4b086596472821f
Noah Chernet <noahchernet@yahoo.com>
2021-09-25 23:03:49 +0300

    Added cases for min or max being negative

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -1,7 +1,14 @@
 #include "main.h"
 
 /**
+ * array_range - creates an array of integers between min and max (including
+ * both min and max)
+ * @min: the smallest and first integer in the array
+ * @max: the biggest and last integer in the array
  *
+ * Return: NULL if min > max
+ *		   NULL if malloc fails to allocate memory
+ *		   array of ints between min and max, including min and max
  */
 
 int *array_range(int min, int max)
@@ -13,7 +20,14 @@ int *array_range(int min, int max)
 	if (min > max)
 		return (NULL);
 
-	arr = malloc (sizeof (int) * (max - min) + 1);
+	if ((min < 0) && (max < 0))
+		j = (-1 * max) - (-1 * min);
+	else if ((max < 0) && (min > 0))
+		j = (-1 * max) - min;
+	else if ((min < 0) && (max > 0))
+		j = max - (-1 * min);
+
+	arr = malloc(sizeof(int) * (max - min) + 1);
 
 	if (!arr)
 		return (NULL);
