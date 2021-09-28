4694a968691002ffd853b7fdf5ad778da60bd649
Noah Chernet <noahchernet@yahoo.com>
2021-09-28 17:40:57 +0300

    Output of 0-main.c that includes dog.h matches task description. .gitignore updated.

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - defines a dog with it's name, age and owner
+ * @name: name of the dog
+ * @age: how old the dog is
+ * @owner: the owner of the dog
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif /* DOG_H */
