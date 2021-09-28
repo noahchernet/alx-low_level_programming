293b5b4e6989b873ee6cb8e6ea1088243f9158e3
Noah Chernet <noahchernet@yahoo.com>
2021-09-28 16:49:08 +0300

    Updated 3-function_like_macro.h

--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -1,14 +1,4 @@
-#ifndef ABS_H
-#define ABS_H
+#ifndef ABS
+#define ABS(X) ((X) < (0) ? (-1 * (X)) : (X))
 
-/**
- * ABS - finds the absolute value of an integer
- * @X: the integer whose absolute value is to be found
- * Return: -1 * X if X < 0, X if X > 0
- */
-int ABS(int X)
-{
-	return ((X) < (0) ? (-1 * (X)) : (X));
-}
-
-#endif /* ABS_H */
+#endif /* ABS */
