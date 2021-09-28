eddbc8a640c6c8a4d6cdbba060a0f6960a1e3ea8
Noah Chernet <noahchernet@yahoo.com>
2021-09-28 22:13:33 +0300

    Handled NULL cases

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -10,17 +10,12 @@
  */
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-	d = malloc(sizeof(struct dog));
-
-	if (d == NULL)
-	{
-		free(d);
-		return;
-	}
-	d->age = 0;
-	d->owner = 0;
 
 	d->name = name;
-	d->owner = owner;
 	d->age = age;
+	d->owner = owner;
+
+	if (d->name == NULL || d->age == 0 || d->owner == NULL)
+		d = NULL;
+
 }
