6354da6a55e7b9471411b504c50ee252ab3a3419
Noah Chernet <noahchernet@yahoo.com>
2021-09-29 15:22:36 +0300

    Output matches task description

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,14 @@
+#include "function_pointers.h"
+
+/**
+ * print_name - prints @name using function @f
+ * @name: name to be printed
+ * @f: function that will print @name
+ *
+ * Return: void
+*/
+
+void print_name(char *name, void (*f)(char *))
+{
+	f(name);
+}
