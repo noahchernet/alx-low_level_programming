3ea61353a8d838904735742a04391773362ac632
Noah Chernet <noahchernet@yahoo.com>
2021-09-30 11:37:02 +0300

    Output of 2-print_strings.c matches task description

---
+++ 0x10-variadic_functions/2-print_strings.c
@@ -0,0 +1,39 @@
+#include "variadic_functions.h"
+
+/**
+ * print_strings - prints all strings passed in as arguments. If one of the
+ * strings is NULL, (nil) is printed instead of it
+ * @separator: the string printed between each passed string
+ * @n: number of arguments
+ *
+ * Return: void
+ */
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+	va_list ap;
+	int i;
+	char *current_str;
+
+	va_start(ap, n);
+
+	if (separator == NULL)
+		separator = "";
+
+	for (i = 0; (unsigned int) i < n; i++)
+	{
+		current_str = va_arg(ap, char *);
+
+		if (current_str == NULL)
+			printf("(nil)");
+
+		else
+			printf("%s", current_str);
+
+		if ((unsigned int) i + 1 == n)
+			break;
+
+		printf("%s", separator);
+	}
+
+	printf("\n");
+}
