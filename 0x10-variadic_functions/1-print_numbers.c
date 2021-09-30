e7a1154b82abae2017327a7f2e4a991ca7565e8f
Noah Chernet <noahchernet@yahoo.com>
2021-09-30 11:26:35 +0300

    Added documentation for 1-print_numbers.c

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -1,7 +1,11 @@
 #include "variadic_functions.h"
 
 /**
+ * print_numbers - prints all the ints passed in as arguments
+ * @separator: the string printed between each passed number
+ * @n: number of arguments passed
  *
+ * Return: void
  */
 
 void print_numbers(const char *separator, const unsigned int n, ...)
