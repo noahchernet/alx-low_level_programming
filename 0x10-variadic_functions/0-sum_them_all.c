00c7c309e3188adba8d7945ce30f076ba9d3a20e
Noah Chernet <noahchernet@yahoo.com>
2021-09-30 11:09:10 +0300

    Included prototypes of all functions in variadic_functions.h. Output o 0-sum_them_all.c matches task description.

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,23 @@
+#include "variadic_functions.h"
+
+/**
+ * sum_them_all - adds all numbers passed in argument
+ * @n: number of arguments passed
+ *
+ * Return: sum of all ints in argument
+ */
+int sum_them_all(const unsigned int n, ...)
+{
+	va_list ap;
+	int i, sum = 0;
+
+	va_start(ap, n);
+
+	if (n == 0)
+		return (0);
+
+	for (i = 0; (unsigned int) i < n; i++)
+		sum += va_arg(ap, int);
+
+	return sum;
+}
