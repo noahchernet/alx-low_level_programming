44b56d02a41a94225249c87a2f296dff618a292f
Noah Chernet <noahchernet@yahoo.com>
2021-10-01 21:07:02 +0300

    Function now returns error with status 99 if operator's string is longer than 1 char

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -22,7 +22,7 @@ int main(int argc, char *argv[])
 	}
 
 	if (*argv[2] != '+' && *argv[2] != '-' && *argv[2] != '*' && *argv[2] !=
-																 '/' && *argv[2] != '%')
+		'/' && *argv[2] != '%' && argv[2][1] != 0)
 	{
 		printf("Error\n");
 		exit(99);
