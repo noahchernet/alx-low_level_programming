2f58d8cd0bec520b3e553f748a7b325e50c15199
Noah Chernet <noahchernet@yahoo.com>
2021-10-01 19:40:50 +0300

    Added case for format being NULL inside main loop

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -13,10 +13,8 @@ void print_all(const char * const format, ...)
 	int i = 0;
 	char *s;
 
-	while (format == NULL)
-		return;
 	va_start(ap, format);
-	while (format[i] != 0)
+	while (format != NULL && format[i] != 0)
 	{
 		switch (format[i])
 		{
