68428c1df1e5cac30a132b34e2b7d8172ddf6bfb
Noah Chernet <noahchernet@yahoo.com>
2021-10-12 23:43:17 +0300

    Output of task 6, 6-pop_listint.c, matches task description

---
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+
+/**
+ * pop_listint - deletes the head node of the passed linked list listint_t
+ * @head: the first element of the linked list
+ *
+ * Return: the number held in the popped element
+ *		   NULL (0) if the passed linked list is NULL
+ */
+
+int pop_listint(listint_t **head)
+{
+	int n = (*head)->n;
+	listint_t *next_node = (*head)->next;
+
+	if (!(*head))
+		return (0);
+
+	free(*head);
+	*head = next_node;
+	return (n);
+
+}
