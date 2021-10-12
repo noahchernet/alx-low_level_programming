31837c1ae72d664d3219083afb0f19114b413055
Noah Chernet <noahchernet@yahoo.com>
2021-10-12 23:18:13 +0300

    Output of task 5, 5-free_listint2.c, matches task description

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,16 @@
+#include "lists.h"
+
+/**
+ * free_listint2 - deletes the list @head from the heap
+ * @head: the list to be deleted
+ *
+ * Return: void
+ */
+
+void free_listint2(listint_t **head)
+{
+	if ((*head)->next)
+		free_listint2(&((*head)->next));
+	free(*head);
+	(*head) = NULL;
+}
