82382f3bc641254a60a321bc3cfa29b1b0fbde41
Noah Chernet <noahchernet@yahoo.com>
2021-10-13 20:00:15 +0300

    Improved NULL check for @head so it doesn't cause segfault when list_len(*head) is run

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -12,12 +12,19 @@
 
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
-	unsigned int i, len = list_len(*head);
-	listint_t *first_node = *head;
-	listint_t *new_node = malloc(sizeof(listint_t));
+	unsigned int i, len;
+	listint_t *first_node;
+	listint_t *new_node;
 	listint_t *next_to_new_node;
 
-	if (!new_node || !head || !*head)
+	if (!head)
+		return (NULL);
+
+	len = list_len(*head);
+	first_node = *head;
+	new_node = malloc(sizeof(listint_t));
+
+	if (!new_node)
 	{
 		free(new_node);
 		return (NULL);
