d1f94ab13c66608635683e2919a904b4c0b2f0d0
Noah Chernet <noahchernet@yahoo.com>
2021-10-13 10:39:50 +0300

    Updated 101-print_number.c

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -1,74 +1,50 @@
 #include "main.h"
-#include "stdio.h"
-
 /**
- * power - raise x to the positive exponent power
- * @x: number to be raised
- * @power: the exponent
- *
- * Return: 1 if power is zero, result if otherwise
- */
-int power(int x, int power)
-{
-	int result = 1;
-/*	printf("%d raised to %d is ", x, power);*/
-
-
-	while (power != 0)
-	{
-		result *= x;
-		--power;
-	}
-
-/*	printf("%d\n", result);*/
-	return (result);
-}
-
-
-/**
- * print_number - prints integer passed to it using _putchar
- * @n: number to be printed
- *
- * Return: void
- */
+  * print_number - prints the passed integer using _putchar
+  * @n: number to be printed
+  *
+  * Return: Always void
+  */
 
 void print_number(int n)
 {
-	int digits = 0, i;
+	int i = 1; /* Holds how many digits n has */
 
-	if (n < 0)
-	{
-		_putchar(45);
-		n *= -1;
-	}
-
-	while (n / power(10, digits) > 0)
-	{
-/*
- *		x = power(10, digits);
- *		printf("%d / %d is %d\n\n", n, x, n / x);
- *		printf("digits = %d, digits - 1 = %d\n", digits, digits - 1);
- */
-		digits++;
-	}
-
-	digits--;
-
+	/**
+	 * if n is zero, just print zero
+	 */
 	if (n == 0)
 	{
 		_putchar(48);
 		return;
 	}
 
-/*
- *	printf("From printf: %d is %c", n, );
- */
-	for (i = digits; i >= 0; i--)
+	/**
+	 * If n is negative, put the '-' sign and replace n by its
+	 * absolute value
+	 */
+	if (n < 0)
 	{
-		_putchar((char) (48 + (n / power(10, i) % 10)));
+		n /= -1;
+		_putchar(45);
+		if (n == -2147483648)
+		{
+			n = 147483648;
+			_putchar(50);
+		}
 	}
 
-/*	printf("---\n%d has %d digits.\n---\n", n, digits);*/
 
+	while (n / i >= 10)
+	{
+		i *= 10;
+	}
+
+	while (i != 0)
+	{
+		_putchar(((n / i) % 10) + 48);
+		i /= 10;
+
+	}
 
 }
