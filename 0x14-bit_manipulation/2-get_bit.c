d3fd6972d5cb6059cb76caa629e7e7ace55f242f
Noah Chernet <noahchernet@yahoo.com>
2021-10-15 15:03:03 -0700

    Added file for task 2

---
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * get_bit - gets the bit of @n at @index
+ * @n: the number
+ * @index: the index to print the bit
+ *
+ * Return:  tha value of the bit at @index or -1 if an error occurs
+ */
+
+int get_bit(unsigned long int n, unsigned int index)
+{
+	int bit = (number >> n) & 1;
+
+	return (bit);
+}
+
