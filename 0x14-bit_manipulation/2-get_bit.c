ea96210fa5eabf4dfae3b65981ff2c2e4aac385b
Noah Chernet <noahchernet@yahoo.com>
2021-10-15 15:51:41 -0700

    Updated 2-get_bit.c

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -10,6 +10,18 @@
 
 int get_bit(unsigned long int n, unsigned int index)
 {
+	unsigned int count = 0;
+	unsigned long int copy = n;
+
+	while (n)
+	{
+		count++;
+		n >>= 1;
+	}
+	n = copy;
+	if (index > count)
+		return (-1);
+
 	int bit = (n >> index) & 1;
 
 	return (bit);
