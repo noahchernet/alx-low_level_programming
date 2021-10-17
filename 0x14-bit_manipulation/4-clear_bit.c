c298f325c1adc1c3ccebb7d052854c93aef1351d
Noah Chernet <noahchernet@yahoo.com>
2021-10-17 20:45:34 +0300

    Returns -1 if memory outside *n is trying to be modified

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -9,6 +9,22 @@
 
 int clear_bit(unsigned long int *n, unsigned int index)
 {
+	unsigned int count = 0;
+	unsigned long copy = *n;
+
+	while (copy)
+	{
+		count++;
+		copy >>= 1;
+	}
+
+	/*
+ * unsigned long *n uses 4 * 8 bits in memory. If index is out of bound
+ * of that memory, don't modify *n and return -1
+ */
+	if (index > 24)
+		return (-1);
+
 	*n &= ~(1UL << index);
 
 	return (1);
