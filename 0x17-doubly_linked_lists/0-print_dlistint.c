3ad9a0c150db9fe6ade5d4c9178631c5ec2d99a4
Noah Chernet <noahchernet@yahoo.com>
2021-11-25 21:19:12 +0300

    Task 0, 0-print_dlistint.c done

---
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+
+/**
+ *
+ */
+size_t print_dlistint(const dlistint_t *h)
+{
+
+	int nodes = 0;
+
+	if (h == NULL)
+		return (0);
+
+	while (h != NULL)
+	{
+		printf("%d\n", h->n);
+		nodes++;
+		h = h->next;
+	}
+
+	return (nodes);
+}
