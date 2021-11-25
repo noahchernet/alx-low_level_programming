4c7c0d529aa1f407cf509066f1af7ac336194531
Noah Chernet <noahchernet@yahoo.com>
2021-11-25 22:19:47 +0300

    Betty linter error fixed for 3-add_dnodeint_end.c

--- 0x17-doubly_linked_lists/3-add_dnodeint_end.c
+++ 0x17-doubly_linked_lists/3-add_dnodeint_end.c
@@ -16,7 +16,7 @@ dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 	if (!new_node)
 	{
 		free(new_node);
-		return NULL;
+		return (NULL);
 	}
 	new_node->n = n;
 	new_node->prev = NULL;
