875dd02f6fd8a24e6ba01a82b390d8f1ac7d7387
Noah Chernet <noahchernet@yahoo.com>
2021-11-26 01:22:50 +0300

    Done task 8, 8-delete_dnodeint.c and added main function for the task in 8-main.c

--- 0x17-doubly_linked_lists/7-main.c
+++ 0x17-doubly_linked_lists/7-main.c
@@ -25,6 +25,9 @@ int main(void)
 	printf("-----------------\n");
 	insert_dnodeint_at_index(&head, 5, 4096);
 	print_dlistint(head);
+	printf("-----------------\n");
+	insert_dnodeint_at_index(&head, 12, 789);
+	print_dlistint(head);
 	free_dlistint(head);
 	head = NULL;
 	return (EXIT_SUCCESS);
