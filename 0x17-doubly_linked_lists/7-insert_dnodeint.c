cc00ce18ca8f37a3c682474fbd8372ef72694f27
Noah Chernet <noahchernet@yahoo.com>
2021-11-26 02:21:36 +0300

    Updated 7-main.c to add more tests 7-insert_dnodeint.c now handle additions of nodes at the end and the beginning of the list

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -18,25 +18,39 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	unsigned int node = 0;
 
 	if (!*h)
-		return (NULL);
-
-	while (*h)
 	{
-		if (idx - 1 == node)
+		*h = new_node;
+		return (new_node);
+	}
+	while (h_copy)
+	{
+		if (idx == node)
 		{
 			new_node->n = n;
-			new_node->prev = (*h);
-			new_node->next = (*h)->next;
-			(*h)->next->prev = new_node;
-			(*h)->next = new_node;
-			*h = h_copy;
-
+			new_node->prev = NULL;
+			new_node->next = NULL;
+			if (!h_copy->prev)
+			{
+				h_copy->prev = new_node;
+				new_node->next = h_copy;
+				*h = new_node;
+				return (new_node);
+			}
+			if (!h_copy->next)
+			{
+				h_copy->next = new_node;
+				new_node->prev = h_copy;
+				return (new_node);
+			}
+			new_node->next = h_copy;
+			new_node->prev = h_copy->prev;
+			h_copy->prev->next = new_node;
+			h_copy->prev = new_node;
 			return (new_node);
 		}
 		node++;
-		*h = (*h)->next;
+		h_copy = h_copy->next;
 	}
-
 	*h = h_copy;
 	return (NULL);
 }
