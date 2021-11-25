cdc80903c6870ed9be61f371fc3abc6788152784
Noah Chernet <noahchernet@yahoo.com>
2021-11-25 23:21:01 +0300

    Added documentation

--- 0x17-doubly_linked_lists/4-free_dlistint.c
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -1,7 +1,10 @@
 #include "lists.h"
 
 /**
+ * free_dlistint - frees the mem occupied by the doubly linked list @head
+ * @head: the first item in the doubly linked list
  *
+ * Return: void
  */
 void free_dlistint(dlistint_t *head)
 {
