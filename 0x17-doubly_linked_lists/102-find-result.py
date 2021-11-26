c81c440cb77a75905061972701d5912b796a5b95
Noah Chernet <noahchernet@yahoo.com>
2021-11-26 20:09:06 +0300

    Adjusted algorithm for finding the biggest palindrome and updated the answer

--- 0x17-doubly_linked_lists/102-find-result.py
+++ 0x17-doubly_linked_lists/102-find-result.py
@@ -2,8 +2,9 @@
 
 # FInd the biggest palindrome made from the product of two 3-digit numbers
 biggest_palindrome = 0
-for x, y in zip(range(100, 1000), range(100, 1000)):
-	if str(x * y)[::-1] == str(x * y):
-		biggest_palindrome = x * y
+for x in range(100, 1000):
+	for y in range(100, 1000):
+		if str(x * y)[::-1] == str(x * y):
+			biggest_palindrome = x * y
 
 print("Biggest palindrome is:", biggest_palindrome)
