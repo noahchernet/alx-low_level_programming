9b7833ddd728e9b76b2adcaf9e93c5c9380fd918
Noah Chernet <noahchernet@yahoo.com>
2021-12-17 19:51:37 +0300

    Redone task 0 and 3

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -9,12 +9,25 @@
  */
 hash_table_t *hash_table_create(unsigned long int size)
 {
-	hash_table_t *new_table = malloc(sizeof(hash_table_t));
+	unsigned long i;
+	hash_table_t *new_table;
 
+	new_table = malloc(sizeof(hash_table_t));
 	if (!new_table)
-		return (NULL);
+		return (0);
 
-	new_table->size = (unsigned long) malloc(sizeof(unsigned long int) * size);
+	new_table->size = size;
+	new_table->array = malloc(sizeof(hash_node_t *) * size);
+	if (!new_table->array)
+	{
+		free(new_table);
+		return (0);
+	}
+
+	for (i = 0; i < size; i++)
+	{
+		new_table->array[i] = NULL;
+	}
 
 	return (new_table);
 }
