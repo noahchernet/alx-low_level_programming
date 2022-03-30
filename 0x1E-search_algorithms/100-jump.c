d5660a0e8b5110e2cecc9565cb9397788f42ec6f
Noah Chernet <noahchernet@yahoo.com>
2022-03-30 10:59:41 +0300

    Added NULL check and value checking for number less than the smallest number in array

--- 0x1E-search_algorithms/100-jump.c
+++ 0x1E-search_algorithms/100-jump.c
@@ -15,14 +15,18 @@ int jump_search(int *array, size_t size, int value)
 	int m = (int) sqrt((double) size);
 	int lo = 0, hi = (int) size - 1;
 
-	for (i = 0; hi < (int) size; i++)
+	for (i = 0; array && hi < (int) size; i++)
 	{
 		lo = i * m;
 		hi = (i + 1) * m;
 
 		printf("Value checked array[%d] = [%d]\n", i * m, array[i * m]);
-		if ((array[lo] <= value && array[hi] >= value) || (array[lo] <= value &&
-		hi >= (int) size))
+
+		if (array[lo] > value)
+			break;
+
+		if ((array[lo] <= value && array[hi] >= value) ||
+			(array[lo] <= value && hi >= (int) size))
 		{
 			printf("Value found between indexes [%d] and [%d]\n", lo, hi);
 
