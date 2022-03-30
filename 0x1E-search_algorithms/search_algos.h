a5c11b4f94c6852c1c01c2718816c50b01a44372
Noah Chernet <noahchernet@yahoo.com>
2022-03-30 10:48:08 +0300

    Trying advanced task #1, jump search algo

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -2,10 +2,11 @@
 #define SEARCH_ALGOS
 
 #include "stdio.h"
+#include "math.h"
 
 int linear_search(int *array, size_t size, int value);
 int binary_search(int *array, size_t size, int value);
-/* void print_array(int *array, size_t size); */
 void print_array(int *array, int lower_bound, int upper_bound);
+int jump_search(int *array, size_t size, int value);
 
 #endif /* SEARCH_ALGOS */
